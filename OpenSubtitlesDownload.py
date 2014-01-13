4a32af36c8bcd6571f15cf00cad4fb43fe3b9ddd
Emeric Grange <emeric.grange@gmail.com>
2014-01-13 18:29:09 +0100

    Bump to bugfix version 3.1

--- OpenSubtitlesDownload.py
+++ OpenSubtitlesDownload.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# OpenSubtitlesDownload.py / Version 3.0
+# OpenSubtitlesDownload.py / Version 3.1
 # https://github.com/emericg/OpenSubtitlesDownload
 # This software is designed to help you find and download subtitles for your favorite videos!
 
@@ -422,7 +422,7 @@ for videoPathDispatch in videoPathList:
 try:
     try:
         # Connection to opensubtitles.org server
-        session = server.LogIn('', '', 'en', 'opensubtitles-download 3.0')
+        session = server.LogIn('', '', 'en', 'opensubtitles-download 3.1')
         if session['status'] != '200 OK':
             superPrint("error", "", "Unable to reach opensubtitles.org servers: " + session['status'] + ".\n\nPlease check:\n- Your Internet connection status\n- www.opensubtitles.org availability\n- Your 200 downloads per 24h limit\n\nThe subtitles search and download service is powered by opensubtitles.org. Be sure to donate if you appreciate the service provided!")
             sys.exit(1)
